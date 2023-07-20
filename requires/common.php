a3604e58c0debe1a99f2db104bc4ddf510034a3b
Htet Aung Linn <htetaunglinn443@gmail.com>
2023-07-20 22:54:43 +0630

    20.07.23

--- requires/common.php
+++ requires/common.php
@@ -1,5 +1,6 @@
 <?php
 $base_url                   = "http://localhost/hotel_booking/";
 $cp_base_url                = "http://localhost/hotel_booking/admin-backend/";
-$common_config['name']      = "Softguide Hotel Booking";
-$common_config['email']     = "softguide@gmail.com";
+$site_config['name']        = "Softguide Hotel Booking";
+$site_config['email']       = "softguide@gmail.com";
+$site_config['key']         = "j7K89Lt35P";
